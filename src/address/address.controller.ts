73817190a159bb3f766d5a8f98d40594ef27244f
fauzanalhfzh <zenbangga@gmail.com>
2025-01-11 02:55:48 +0700

    FEAT: adding API consumes and interceptors

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
+import {
+  ApiConsumes,
+  ApiOperation,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AddressService } from './address.service';
 import { AddressResponse, CreateAddressRequest } from 'src/model/address.model';
 import { WebResponse } from 'src/model/web.model';
 import { Auth } from 'src/common/auth.decorator';
 import { User } from '@prisma/client';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('Address User')
 @Controller('/api/v1/users/addresses')
@@ -15,6 +27,8 @@ export class AddressController {
   @HttpCode(200)
   @ApiSecurity('Authorization')
   @ApiOperation({ summary: 'Create address for users' })
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
   async create(
     @Auth() user: User,
     @Body() request: CreateAddressRequest,
