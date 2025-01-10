318c671b010b918a56c138b32ffe16dbbdc7f6b7
fauzanalhfzh <zenbangga@gmail.com>
2025-01-11 02:56:18 +0700

    FEAT: adding API consumes and interceptors

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,10 +6,11 @@ import {
   HttpCode,
   Patch,
   Post,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { WebResponse } from '../model/web.model';
-import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { ApiConsumes, ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
 import {
   LoginUserRequest,
   RegisterUserRequest,
@@ -18,6 +19,7 @@ import {
 } from '../model/user.model';
 import { Auth } from '../common/auth.decorator';
 import { User } from '@prisma/client';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('Users')
 @Controller('/api/v1/users')
@@ -26,11 +28,14 @@ export class UserController {
 
   @Post('/auth/register')
   @HttpCode(200)
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
   @ApiOperation({ summary: 'Register new user' })
   async register(
     @Body() request: RegisterUserRequest,
   ): Promise<WebResponse<UserResponse>> {
     const result = await this.userService.register(request);
+    console.log(request);
     return {
       data: result,
     };
@@ -39,6 +44,8 @@ export class UserController {
   @Post('/auth/login')
   @HttpCode(200)
   @ApiOperation({ summary: 'Login users' })
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
   async login(
     @Body() request: LoginUserRequest,
   ): Promise<WebResponse<UserResponse>> {
@@ -52,6 +59,8 @@ export class UserController {
   @HttpCode(200)
   @ApiSecurity('Authorization')
   @ApiOperation({ summary: 'Get user data' })
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
   async get(@Auth() user: User): Promise<WebResponse<UserResponse>> {
     const result = await this.userService.get(user);
     return {
@@ -63,6 +72,8 @@ export class UserController {
   @HttpCode(200)
   @ApiSecurity('Authorization')
   @ApiOperation({ summary: 'Update data users' })
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
   async update(
     @Auth() user: User,
     @Body() request: UpdateUserRequest,
@@ -77,6 +88,8 @@ export class UserController {
   @HttpCode(200)
   @ApiSecurity('Authorization')
   @ApiOperation({ summary: 'Logout users' })
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
   async logout(@Auth() user: User): Promise<WebResponse<boolean>> {
     await this.userService.logout(user);
     return {
