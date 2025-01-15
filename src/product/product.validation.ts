939a0dead10378ebf78b4274e2e1d65b94249432
fauzanalhfzh <zenbangga@gmail.com>
2025-01-15 16:39:09 +0700

    UPDATE: adding optional to discount and clear all typo

--- src/product/product.validation.ts
+++ src/product/product.validation.ts
@@ -7,16 +7,17 @@ export class ProductValidation {
     price: z.number().min(1),
     stock: z.number().min(1),
     netto: z.number().min(1),
-    discount: z.number().min(1),
+    discount: z.number().min(1).optional(),
     merchant_id: z.string().min(1).max(150),
     category_id: z.string().min(1).max(150),
   });
   static readonly UDPATE_PRODUCT: ZodType = z.object({
-    product_name: z.string().min(1).max(100).optional(),
+    name: z.string().min(1).max(100).optional(),
     description: z.string().min(1).max(150).optional(),
     price: z.number().min(1).optional(),
     stock: z.number().min(1).optional(),
     netto: z.number().min(1).optional(),
     discount: z.number().min(1).optional(),
+    category_id: z.string().min(1).max(150).optional(),
   });
 }
