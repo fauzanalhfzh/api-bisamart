77f37f312227f68e426725ed1afc67ffdcf3d95f
fauzanalhfzh <zenbangga@gmail.com>
2025-01-15 16:35:27 +0700

    UPDATE: Delete country and adding email validation

--- src/courier/courier.validation.ts
+++ src/courier/courier.validation.ts
@@ -4,10 +4,9 @@ import { z, ZodType } from 'zod';
 export class CourierValidation {
   static readonly REGISTER: ZodType = z.object({
     name: z.string().min(1).max(100),
-    email: z.string().min(1).max(50),
+    email: z.string().min(1).max(50).email(),
     phone_number: z.string().min(1).max(50),
     password: z.string().min(1).max(100),
-    country: z.string().min(1).max(50),
     address_ktp: z.string().min(1).max(150),
     ktp: z.string().min(1).max(30),
     vehicle_brand: z.string().min(1).max(50),
