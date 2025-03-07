091961611c149613d78c30c1a4d41f31d0b2f8e8
fauzanalhfzh <zenbangga@gmail.com>
2025-03-07 15:00:57 +0700

    UPDATE: adding category and address module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { SimpleModule } from './simple/simple.module';
+import { CategoryModule } from './category/category.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
   imports: [
     CommonModule,
     SimpleModule,
     UserModule,
+    AddressModule,
+    CategoryModule,
   ],
   controllers: [],
   providers: [],
