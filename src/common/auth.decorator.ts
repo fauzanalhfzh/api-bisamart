f921b64c14054d48449e3982e0472da8e594b555
fauzanalhfzh <zenbangga@gmail.com>
2025-08-18 14:10:04 +0700

    change middleware

--- src/common/auth.decorator.ts
+++ src/common/auth.decorator.ts
@@ -8,10 +8,12 @@ export const Auth = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     const user = request.user;
+    
+    // Mengembalikan user jika ada, atau null jika tidak ada
     if (user) {
       return user;
-    } else {
-      throw new HttpException('Unauthorized', 401);
     }
+    return null;  // Atau bisa undefined jika ingin lebih eksplisit
   },
 );
+ 
